91b9383ba11183631107bbcb2bd44e1d33563817
endigma <endigma@mailcat.ca>
2024-08-10 16:59:13 -0300

    switch to cf pages and update

--- astro.config.ts
+++ astro.config.ts
@@ -1,21 +1,21 @@
-import mdx from "@astrojs/mdx"
-import svelte from "@astrojs/svelte"
-import { defineConfig } from "astro/config"
-import uno from "unocss/astro"
+import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
+import { defineConfig } from "astro/config";
+import uno from "unocss/astro";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://endigma.dev",
-	markdown: {
-		shikiConfig: {
-			theme: "github-dark-dimmed",
-		},
-	},
-	integrations: [
-		uno({
-			injectReset: true,
-		}),
-		mdx(),
-		svelte(),
-	],
-})
+    site: "https://endigma.dev",
+    markdown: {
+        shikiConfig: {
+            theme: "github-dark-dimmed"
+        }
+    },
+    integrations: [uno({
+        injectReset: true
+    }), mdx(), svelte()],
+    output: "server",
+    adapter: cloudflare()
+});
